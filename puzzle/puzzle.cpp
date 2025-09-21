7f774504168cba3816c053c24bd40ee2458a538f
Abin Jo <abin1303@naver.com>
2025-09-21 19:23:20 +0900

    puzzle_main

--- puzzle/puzzle.cpp
+++ puzzle/puzzle.cpp
@@ -2,6 +2,7 @@
 #include "ui_puzzle.h"
 #include "puzzleselectdialog.h"
 #include <QMessageBox>
+#include <QDebug>
 
 puzzle::puzzle(QWidget *parent)
     : QWidget(parent)
@@ -17,12 +18,34 @@ puzzle::~puzzle()
 
 void puzzle::on_cameraButton_clicked()
 {
-    emit switchToWebcam();   // main.cpp 에서 이걸 받아서 페이지 전환
+    // 다이얼로그 생성
+    PuzzleSelectDialog dlg(this);
+    if (dlg.exec() == QDialog::Accepted) {
+        int type = dlg.selectedPuzzleType();
+        qDebug() << "카메라 버튼 → 선택된 퍼즐 타입:" << type;
+
+        if (type == 5) {
+            QMessageBox::information(this, "선택 결과", "5x5 퍼즐 선택됨!");
+        } else if (type == 8) {
+            QMessageBox::information(this, "선택 결과", "8x8 퍼즐 선택됨!");
+        }
+    }
 }
 
 
 void puzzle::on_imageButton_clicked()
 {
+    PuzzleSelectDialog dlg(this);
+    if (dlg.exec() == QDialog::Accepted) {
+        int type = dlg.selectedPuzzleType();
+        qDebug() << "이미지 버튼 → 선택된 퍼즐 타입:" << type;
+
+        if (type == 5) {
+            QMessageBox::information(this, "선택 결과", "5x5 퍼즐 선택됨!");
+        } else if (type == 8) {
+            QMessageBox::information(this, "선택 결과", "8x8 퍼즐 선택됨!");
+        }
+    }
 
 }
 
