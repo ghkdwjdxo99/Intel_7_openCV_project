9634c54b2b2c27f163d0953dccf57b19d4db5feb
seol1006a <seol1006@gmail.com>
2025-09-23 11:52:25 +0900

    퍼즐이 완성?

--- puzzle/playpage.h
+++ puzzle/playpage.h
@@ -4,6 +4,14 @@
 #include <QWidget>
 #include <QTimer>
 
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsPixmapItem>
+#include <QVector>
+#include <QSize>
+#include <QPointF>
+#include <QRectF>
+
 namespace Ui {
 class PlayPage;
 }
@@ -19,6 +27,7 @@ public:
 
 protected:
     void showEvent(QShowEvent *event) override;
+    bool eventFilter(QObject *watched, QEvent *event) override;
 
 private slots:
     void on_StopBT_clicked();
@@ -37,6 +46,26 @@ private:
     QTimer *timer;
     int elapsedSeconds;
     int hintCount;
+    void trySnap(QGraphicsPixmapItem *piece, double tolerance);
+
+    // 🎯 퍼즐판을 그릴 QGraphicsScene
+    QGraphicsScene *mScene = nullptr;
+
+    // 🎯 슬롯(퍼즐 보드 칸) 저장 (나중에 스냅/정답 판정에 씀)
+    QVector<QGraphicsRectItem*> mSlots;
+
+    // 🎯 조각 저장 (Step 2에서 단순히 드래그만, Step 3부터 스냅에 활용)
+    QVector<QGraphicsPixmapItem*> mPieces;
+
+    // 🎯 퍼즐판 행/열, 셀 크기, 좌상단 위치 (초기값은 임시로 세팅)
+    int mRows = 4;
+    int mCols = 6;
+    QSize mCellSize = QSize(96, 96);
+    QPointF mTopLeft = QPointF(40, 40);
+
+    // 🎯 조각을 무작위로 뿌릴 영역 (퍼즐판 오른쪽 여백)
+    QRectF mRandomArea;
+
 };
 
 #endif // PLAYPAGE_H
